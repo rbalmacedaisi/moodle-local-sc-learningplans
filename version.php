881428313916e8e9f9768df2473d6999b6b0df9d
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-02-26 14:31:11 -0500

    fx

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_sc_learningplans';
-$plugin->release = '1.0.1';
-$plugin->version = 2026021200;
+$plugin->release = '1.0.2';
+$plugin->version = 2026022600;  // Added local_learningplan_deps table for plan dependencies
 $plugin->requires = 2020061500;
